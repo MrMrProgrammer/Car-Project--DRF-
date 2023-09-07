56d3758f5c4a36561bf0f943016d3f03566244b6
MrMrProgrammer <mr.mr.programmer@gmail.com>
2023-09-07 12:02:48 +0330

    ok

--- car/models.py
+++ car/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Car(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     brand = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.IntegerField()
